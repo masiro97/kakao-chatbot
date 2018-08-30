0ba7d581461c2a788c7fc6d0aab76890fb69b06d
Hyeyoung Shin <kscr37@naver.com>
2018-08-30 06:38:11 +0000

    movie API

--- app.py
+++ app.py
@@ -1,5 +1,7 @@
 from flask import Flask, request, jsonify
+from bs4 import BeautifulSoup
 import os
+# json으로 바꾸기 위해 라이브러리 추가
 import json
 import random
 import requests
@@ -52,7 +54,32 @@ def message():
         res = requests.get(url).json()
         return_msg = "나만 고양이 없어:("
         img_url = res[0]['url']
+    
+    elif msg == "영화":
+        url = "https://movie.naver.com/movie/running/current.nhn"
+        res = requests.get(url).text
+        doc = BeautifulSoup(res, "html.parser")
         
+        title_tag = doc.select('dt.tit > a')
+        star_tag = doc.select('div.star_t1 > a > span.num')
+        reserve_tag = doc.select('div.star_t1.b_star > span.num')
+        img_tag = doc.select('div > a > img')
+        
+        movie_dic = {}
+
+        for i in range(0,10):
+            movie_dic[i+1] = {
+                "title" : title_tag[i].text,
+                "star" : star_tag[i].text,
+                "reserve" : reserve_tag[i].text,
+                "img_url" : img_tag[i]['src'] # img_tag[i].get('src')
+            }
+        
+        pick_movie = movie_dic[random.randrange(0,10)]
+        
+        return_msg = "{} || 별점 : {} 예매율 : {}".format(pick_movie["title"],pick_movie["star"],pick_movie['reserve'])
+        img_bool = True
+        img_url = pick_movie["img_url"]
     else:
         return_msg = "현재 메뉴만 지원합니다."
     
--- test.py
+++ test.py
@@ -1,5 +1,54 @@
 import requests
+from bs4 import BeautifulSoup
+import random
+import re
 
-url = "https://api.thecatapi.com/v1/images/search?mime_types=jpg"
-res = requests.get(url).json()
-print(res[0]['url'])
+url = "https://movie.naver.com/movie/running/current.nhn"
+res = requests.get(url).text
+doc = BeautifulSoup(res, "html.parser")
+
+# pick = doc.select("#content > div.article > div > div.lst_wrap > ul > li > dl > dt > a")
+# for movieNm in pick:
+#     print(movieNm.text)
+
+# movieNm_doc = doc.select('dt.tit > a')
+# movieResv_doc = doc.select('div.star_t1 > a > span.num')
+# movieStar_doc = doc.select('div.star_t1.b_star > span.num')
+
+# movieNm_list = []
+# movieResv_list = []
+# movieStar_list = []
+
+# for movieNm in movieNm_doc:
+#     movieNm_list.append(movieNm.text)
+#     # print(movieNm.text)
+    
+# for movieResv in movieResv_doc:
+#     movieResv_list.append(movieResv.text)
+
+# for movieStar in movieStar_doc:
+#     movieStar_list.append(movieStar.text)
+    
+# print(movieStar_list)
+
+title_tag = doc.select('dt.tit > a')
+star_tag = doc.select('div.star_t1 > a > span.num')
+reserve_tag = doc.select('div.star_t1.b_star > span.num')
+img_tag = doc.select('div > a > img')
+# print(len(title_tag))
+# print(len(star_tag))
+# print(len(reserve_tag))
+
+movie_dic = {}
+
+for i in range(0,10):
+    movie_dic[i+1] = {
+        "title" : title_tag[i].text,
+        "star" : star_tag[i].text,
+        "reserve" : reserve_tag[i].text,
+        "img_url" : img_tag[i]['src'] # img_tag[i].get('src')
+    }
+
+pick_movie = movie_dic[random.randrange(0,10)]
+
+print(pick_movie)
